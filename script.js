e2740835fc7934d9e87dd391f555d7b7e5c04073
rudrachourey <116058198+rudrachourey@users.noreply.github.com>
2023-02-12 01:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector("#manu1").addEventListener("click",function(){
+    document.querySelector(".table").style.rotate = "0deg" 
+    document.querySelector("#overlay").style.left= "17px"
+    document.querySelector(".rating h2").textContent = "2.5"
+    document.querySelector(".about span").textContent = "Ayush gupta"
+    document.querySelector("#text1").style.top = "0%" 
+    document.querySelector("#text2").style.top = "-100%" 
+    document.querySelector("#text3").style.top = "100%" 
+    document.querySelector("#text4").style.top = "-100%" 
+    document.querySelector("#box").style.backgroundColor = "#f2b58f98" 
+
+
+})
+document.querySelector("#manu2").addEventListener("click",function(){
+    document.querySelector(".table").style.rotate = "90deg"
+    document.querySelector("#overlay").style.left= "26%"
+    document.querySelector(".rating h2").textContent = "5.5"
+    document.querySelector(".about span").textContent = "Rudra Chourey" 
+    document.querySelector("#text1").style.top = "-100%" 
+    document.querySelector("#text2").style.top = "0%" 
+    document.querySelector("#text3").style.top = "100%" 
+    document.querySelector("#text4").style.top = "-100%" 
+    document.querySelector("#box").style.backgroundColor = "#70f1f899" 
+})
+document.querySelector("#manu3").addEventListener("click",function(){
+    document.querySelector(".table").style.rotate = "180deg" 
+    document.querySelector("#overlay").style.left= "52%"
+    document.querySelector(".rating h2").textContent = "3.2"
+    document.querySelector(".about span").textContent = "sahil saxena"
+    document.querySelector("#text3").style.top = "0%" 
+    document.querySelector("#text2").style.top = "100%" 
+    document.querySelector("#text4").style.top = "-100%" 
+    document.querySelector("#text1").style.top = "100%" 
+    document.querySelector("#box").style.backgroundColor = "#4fa4ff99" 
+
+})
+document.querySelector("#manu4").addEventListener("click",function(){
+    document.querySelector(".table").style.rotate = "270deg" 
+    document.querySelector("#overlay").style.left= "77%"
+    document.querySelector(".rating h2").textContent = "4.9"
+    document.querySelector(".about span").textContent = "Yash mainx the great"
+    document.querySelector("#text4").style.top = "0%" 
+    document.querySelector("#text3").style.top = "-100%" 
+    document.querySelector("#text2").style.top = "100%" 
+    document.querySelector("#text1").style.top = "-100%" 
+    document.querySelector("#box").style.backgroundColor = " #e72b3599" 
+})
+
